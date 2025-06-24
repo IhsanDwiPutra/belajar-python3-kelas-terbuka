2e530f3763fa33bd96d7ce855814edb2a76727d8
IhsanDwiPutra <ihsandwipurnamaputra@gmail.com>
2025-06-24 21:11:14 +0700

    Tuples dan Set

---
+++ 37-Tuples dan Set/main.py
@@ -0,0 +1,57 @@
+# List
+
+data_list = [1,4,3,5,3] # menggunakan kurung siku
+print(data_list)
+# tuples
+data_tuples = (4,5,3,5,2)
+print(data_tuples[0])
+
+# tidak bisa dilakukan
+# data_tuples[0] = 1 
+# data_tuples.append(3) tidak punya atribut
+
+# sets (himpunan)
+# tidak punya urutan
+data_sets = {10,2,4,2,2,5}
+print(data_sets)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
